75de8958007728f3a5ea142dd7e9f960f094a1a3
Michael Carr <xyeres@me.com>
2021-03-22 15:31:43 -0700

    Add cards

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { response } = require("express");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -9,44 +8,26 @@ app.use(cookieParser())
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello');
-    }
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello')
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
-app.get('/hello', (req, res) => {
-    if (!req.cookies.username) {
-        res.render('hello');
-    } else {
-        res.redirect('/')
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-
-});
-
-app.get('/cards', (req, res) => {
-    res.render('card', { prompt: "Who is buried in grants two?", colors})
-});
-
-app.get('/names', (req, res) => {
-    res.render('names', {names})
-})
-
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log('the aplication is running on localhost:3001');
 });
-
 
